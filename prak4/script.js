591ecab2176af0c9e8f1f1f5a42b90c864e1b433
Dhio Eko Permana_121140086 <118009106+dhio-18@users.noreply.github.com>
2023-11-08 22:22:19 +0700

    Add files via upload

---
+++ prak4/script.js
@@ -0,0 +1,30 @@
+function ganjilgenap(){
+    // deklarasi variabel mengambil element berdasarkan nama id
+    let inputvalue = document.getElementById('inputvalue').value;
+    let input = parseInt(inputvalue);
+    let ganjil = document.getElementById('ganjil');
+    let genap = document.getElementById('genap');
+
+    // proses perhitungan jumlah ganjil gennap
+
+    // jika input > 0 makan akan di proses
+    // jika tidak makan akan ke bagian else menampilakn alert
+    if (input > 0){
+        // deklarasi variabel
+        let outGanjil = 0;
+        let outGenap = 0;
+        // melakukan perulangan jika variabel i habis di bagi 2 maka variabel outgenap bertambah 
+        // dan juga sebalikanya jika variabel i tidak habis dibagi dua makan outganjil bertambah
+        for(let i = 1; i <= input; i++){
+            if(i % 2 == 0){
+                outGenap++
+            }else{
+                outGanjil++
+            }
+        }
+        ganjil.textContent = outGanjil;
+        genap.textContent = outGenap;
+    }else{
+        alert('Gagal');
+    }
+}
